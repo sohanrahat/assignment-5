2938a3650633c9d88aa08bd577e2ce8e9dc4f2f7
sohanrahat <sohanboy@gmail.com>
2025-03-01 19:29:08 +0600

    updated the button styles

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -6,3 +6,20 @@ function getRandomColor() {
     }
     return color;
 }
+
+// current date function 
+
+function updateDate() {
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const now = new Date();
+
+    const day = days[now.getDay()]; // Get current day (e.g., Wed)
+    const date = now.toLocaleDateString('en-US', {
+        month: 'short', day: '2-digit', year: 'numeric'
+    }); // Get formatted date (e.g., Jul 28 2025)
+
+    document.getElementById('day').textContent = day;
+    document.getElementById('full-date').textContent = date;
+}
+
+updateDate(); // Call function to update the date
