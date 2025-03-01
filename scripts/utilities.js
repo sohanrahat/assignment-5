d4a94a42c68ecbd5274aee920f82e0054f1bd3c6
sohanrahat <sohanboy@gmail.com>
2025-03-01 20:17:41 +0600

    assigned buttons to the work

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,25 +1,26 @@
-function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+
+
+// assigning buttons to the work 
+
+document.body.addEventListener("click", function (event) {
+    if (event.target.classList.contains("task-btn")) {
+        let button = event.target;
+        button.disabled = true; 
+        button.style.backgroundColor = #9BA8F8;
+
+        let counterElement = document.getElementById('task-counter');
+        counterElement.textContent = parseInt(counterElement.textContent) + 1;
+
+        let taskTitle = button.closest('.card-body').querySelector('.card-title').textContent;
+
+        let activityLog = document.getElementById('activity-log');
+        let newEntry = document.createElement('p');
+        newEntry.textContent = `You have Complete The Task: ${taskTitle}`;
+        newEntry.classList.add('bg-[#f47ff]', 'p-2','rounded-md','font-regular','text-sm'); 
+        activityLog.appendChild(newEntry);
+    
     }
-    return color;
-}
-
-// current date function 
-
-function updateDate() {
-    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    const now = new Date();
-
-    const day = days[now.getDay()]; // Get current day (e.g., Wed)
-    const date = now.toLocaleDateString('en-US', {
-        month: 'short', day: '2-digit', year: 'numeric'
-    }); // Get formatted date (e.g., Jul 28 2025)
-
-    document.getElementById('day').textContent = day;
-    document.getElementById('full-date').textContent = date;
-}
-
-updateDate(); // Call function to update the date
+});
+    
+    
+    )
